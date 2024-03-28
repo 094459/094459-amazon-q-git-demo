8226fb53f28e249139c426f0257ddc818f4878b1
Ricardo Sueiras <ricardo.sueiras@gmail.com>
2024-03-28 10:06:03 +0000

    Add a new endpoint that is insecure

---
+++ src/main/java/uk/co/beachgeek/demo/UserProfileRestController.java
@@ -0,0 +1,30 @@
+package uk.co.beachgeek.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+@RestController
+public class UserProfileRestController {
+
+    @GetMapping("/api/updateProfile")
+    public ResponseEntity<?> updateProfile(@RequestBody UserProfileUpdateRequest request) {
+        // Assume there's a service to update the user's email
+        // This example directly returns a response entity for simplicity
+        return ResponseEntity.ok("Profile updated successfully");
+    }
+
+    static class UserProfileUpdateRequest {
+        private String email;
+
+        // Getters and Setters
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+    }
+}
